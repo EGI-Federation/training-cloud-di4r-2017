3ae969af80dbd5932b7f74d01ceb9bd1cafb9c37
glarocca <giuseppe.larocca@ct.infn.it>
2016-09-02 08:05:54 +0000

    Changed 'provider_template:resource_id' with 'provider_template:resource_name'

--- pyEGIAppDB/pyEGI_AppDB.py
+++ pyEGIAppDB/pyEGI_AppDB.py
@@ -22,8 +22,8 @@ import xmltodict
 
 __author__    = "Giuseppe LA ROCCA"
 __email__     = "giuseppe.larocca@egi.eu"
-__version__   = "$Revision: 0.0.7"
-__date__      = "$Date: 23/08/2016 08:50:22"
+__version__   = "$Revision: 0.0.8"
+__date__      = "$Date: 02/09/2016 10:03:27"
 __copyright__ = "Copyright (c) 2016 EGI Foundation"
 __license__   = "Apache Licence v2.0"
 
@@ -39,33 +39,33 @@ def appdb_call(c):
 
 def get_provider_metadata(id):
 
-        try:
+	try:
                 # E.g.: https://appdb.egi.eu/rest/1.0/va_providers/8253G0
-                data = appdb_call('/rest/1.0/va_providers/%s' %id)
+        	data = appdb_call('/rest/1.0/va_providers/%s' %id)
+		
+		provider_name = data['appdb:appdb']['virtualization:provider']['provider:name']	
 
-                provider_name = data['appdb:appdb']['virtualization:provider']['provider:name']
+		if (data['appdb:appdb']['virtualization:provider'].has_key('provider:endpoint_url')):
+			provider_endpoint_url = data['appdb:appdb']['virtualization:provider']['provider:endpoint_url']
+		else:
+			provider_endpoint_url = 'N/A' 
+			
+		site_name = data['appdb:appdb']['virtualization:provider']['appdb:site']['site:officialname']
+		status = data['appdb:appdb']['virtualization:provider']['appdb:site']
+		url = ("https://appdb.egi.eu/rest/1.0/va_providers/%s" %id)
 
-                if (data['appdb:appdb']['virtualization:provider'].has_key('provider:endpoint_url')):
-                        provider_endpoint_url = data['appdb:appdb']['virtualization:provider']['provider:endpoint_url']
-                else:
-                        provider_endpoint_url = 'N/A'
+	except:
+        	print ""
 
-                site_name = data['appdb:appdb']['virtualization:provider']['appdb:site']['site:officialname']
-                status = data['appdb:appdb']['virtualization:provider']['appdb:site']
-                url = ("https://appdb.egi.eu/rest/1.0/va_providers/%s" %id)
-
-        except:
-                print ""
-
-        print "\n\n %s [%s] " %(provider_name, id)
-        print " \t--> Sitename: %s \
-                \n\t--> Endpoint: %s \
+	print "\n\n %s [%s] " %(provider_name, id)
+	print " \t--> Sitename: %s \
+		\n\t--> Endpoint: %s \
                 \n\t--> Status: %s \
                 \n\t--> URL: %s" \
-                % (site_name,
+		% (site_name,
                    provider_endpoint_url,
-                   status['@status'],
-                   url)
+		   status['@status'],
+		   url)
 
 
 def get_resource(id):
@@ -75,7 +75,12 @@ def get_resource(id):
                 data = appdb_call('/rest/1.0/va_providers/%s' %id)
 
                 for resource_tpl in data['appdb:appdb']['virtualization:provider']['provider:template']:
-                        print "\t%s" %resource_tpl['provider_template:resource_id']
+                        #print "\t%s" %resource_tpl['provider_template:resource_name']
+                        if resource_tpl['provider_template:resource_name'].startswith("http://"):
+				resource_tpl = resource_tpl['provider_template:resource_name'].split("resource_tpl#",1)
+			        print "\tresource_tpl#%s" %resource_tpl[1]
+			else:
+                        	print "\t%s" %resource_tpl['provider_template:resource_name']
 
         except:
                 print ""
@@ -87,14 +92,14 @@ def get_va(id):
                 # E.g.: https://appdb.egi.eu/rest/1.0/va_providers/8253G0
                 data = appdb_call('/rest/1.0/va_providers/%s' %id)
 
-                for os_tpl in data['appdb:appdb']['virtualization:provider']['provider:image']:
-                        try:
-                                if vo in os_tpl['@voname']:
-                                        print "\t - Name = %s [v%s] " %(os_tpl['@appname'], os_tpl['@vmiversion'])
-                                        print "\t - OCCI ID = %s" %os_tpl['@va_provider_image_id']
-                                        print "\t - URI = %s" %os_tpl['@mp_uri']
-                        except:
-                                print ""
+		for os_tpl in data['appdb:appdb']['virtualization:provider']['provider:image']:
+			try:
+				if vo in os_tpl['@voname']:
+					print "\n\t - Name = %s [v%s] " %(os_tpl['@appname'], os_tpl['@vmiversion'])
+        				print "\t - OCCI ID = %s" %os_tpl['@va_provider_image_id']
+        				print "\t - URI = %s" %os_tpl['@mp_uri']
+        		except:
+        			print ""
 
         except:
                 print ""
@@ -104,31 +109,31 @@ def get_va(id):
 def main():
 
         print "\n ~ Listing providers that have subscribed the [%s] VO" %vo
+	
+	try:
+		# E.g. https://appdb.egi.eu/rest/1.0/sites?flt=%%2B%%3Dvo.name:training.egi.eu&%%2B%%3Dsite.supports:1
+	        data = appdb_call('/rest/1.0/sites?flt=%%2B%%3Dvo.name:%s&%%2B%%3Dsite.supports:1' %vo)
 
-        try:
-                # E.g. https://appdb.egi.eu/rest/1.0/sites?flt=%%2B%%3Dvo.name:training.egi.eu&%%2B%%3Dsite.supports:1
-                data = appdb_call('/rest/1.0/sites?flt=%%2B%%3Dvo.name:%s&%%2B%%3Dsite.supports:1' %vo)
+		providersID = []
+		for site in data['appdb:appdb']['appdb:site']:
+        	        if  type(site['site:service']) == type([]):
+        			for service in site['site:service']:
+					#print "%s" %service['@id']
+					providersID.append(service['@id'])
+			else:
+				providersID.append(site['site:service']['@id'])
 
-                providersID = []
-                for site in data['appdb:appdb']['appdb:site']:
-                        if  type(site['site:service']) == type([]):
-                                for service in site['site:service']:
-                                        #print "%s" %service['@id']
-                                        providersID.append(service['@id'])
-                        else:
-                                providersID.append(site['site:service']['@id'])
+		# Get provider metadata
+		for ID in providersID:
+			get_provider_metadata(ID)        	
+			print "\n ~ Listing available resource(s) templates"
+			get_resource(ID)
+			print "\n ~ Listing available Virtual Appliance(s)"
+			get_va(ID)
+        		print ""
 
-                # Get provider metadata
-                for ID in providersID:
-                        get_provider_metadata(ID)
-                        print "\n ~ Listing available resource(s) templates"
-                        get_resource(ID)
-                        print "\n ~ Listing available Virtual Appliance(s)"
-                        get_va(ID)
-                        print ""
-
-        except:
-                print ""
+	except:
+        	print ""
 
 if __name__ == "__main__":
         main()
