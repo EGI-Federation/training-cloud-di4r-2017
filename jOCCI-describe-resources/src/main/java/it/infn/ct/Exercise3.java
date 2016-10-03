60aaac1dcb3785e2e7e76030c6c9302a8232dbb8
glarocca <giuseppe.larocca@ct.infn.it>
2016-10-03 09:12:42 +0000

    Removed comments

--- jOCCI-describe-resources/src/main/java/it/infn/ct/Exercise3.java
+++ jOCCI-describe-resources/src/main/java/it/infn/ct/Exercise3.java
@@ -183,7 +183,6 @@ public class Exercise3
 	// [ Setting preferences here! ]
 	String AUTH = "x509"; 
         String OCCI_ENDPOINT_HOST = "https://carach5.ics.muni.cz:11443"; // <= Change here!
-        //String OCCI_ENDPOINT_HOST = "https://cloud.ifca.es:8787/occi1.1"; // <= Change here!
 
         String TRUSTED_CERT_REPOSITORY_PATH = "/etc/grid-security/certificates";
         String PROXY_PATH = "/tmp/x509up_u1000"; // <= Change here!
